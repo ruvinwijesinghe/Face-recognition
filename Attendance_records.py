08d0a90d4784498d4cda1d615f955227ebaec0f6
Ruvin Wijesinghe <ruvinwijesinghe@Ruvins-MacBook-Air.local>
2022-04-12 18:20:33 +0530

    Customization started

--- Attendance_records.py
+++ Attendance_records.py
@@ -3,6 +3,10 @@ import face_recognition
 import numpy as np
 import os
 from datetime import datetime
+from keras.models import load_model
+from time import sleep
+from keras.preprocessing.image import img_to_array
+from keras.preprocessing import image
 
 path = 'Image_records'
 images = []
@@ -34,11 +38,16 @@ def markAttendance(name):
         if name not in nameList:
             now = datetime.now()
             dtString = now.strftime('%H:%M:%S')
-            f.writelines(f'\n{name},{dtString}')
+            f.writelines(f'\n{name},{dtString},{label}')
 
 
 encodeListKnown = findEncodings(images)
 print('Encoding found and completed')
+
+face_classifier = cv2.CascadeClassifier(r'C:\Users\Ruvin\Desktop\Face-recognition-main\haarcascade_frontalface_default.xml')
+classifier =load_model(r'C:\Users\Ruvin\Desktop\Face-recognition-main\model.h5')
+
+emotion_labels = ['Angry','Disgust','Fear','Happy','Neutral', 'Sad', 'Surprise']
 
 cap = cv2.VideoCapture(0)
 
@@ -49,13 +58,29 @@ while True:
 
     facesCurFrame = face_recognition.face_locations(imgs)
     encodeCurFrame = face_recognition.face_encodings(imgs,facesCurFrame)
-
+    _, frame = cap.read()
+    labels = []
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray)
     for encodeFace,faceLoc in zip(encodeCurFrame,facesCurFrame):
         matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
         faceDis = face_recognition.face_distance(encodeListKnown,encodeFace)
         print(faceDis)
         matchIndex = np.argmin(faceDis)
+        for (x,y,w,h) in faces:
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2)
+            roi_gray = gray[y:y+h,x:x+w]
+            roi_gray = cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
+            if np.sum([roi_gray])!=0:
+                roi = roi_gray.astype('float')/255.0
+                roi = img_to_array(roi)
+                roi = np.expand_dims(roi,axis=0)
 
+                prediction = classifier.predict(roi)[0]
+                label=emotion_labels[prediction.argmax()]
+                label_position = (x,y-8)
+                cv2.putText(frame,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+            
         if matches[matchIndex]:
             name = classNames[matchIndex].upper()
             print(name)
@@ -63,7 +88,7 @@ while True:
             y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
             cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
             cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
-            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+            cv2.putText(img,f'{name},{label}',(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
             markAttendance(name)
     cv2.imshow('Webcam',img)
     cv2.waitKey(1)
